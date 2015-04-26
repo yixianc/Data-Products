3bb0b4a20da58083968b999995abded5c28f0f3f
yixianc <chuayx@hotmail.com>
2015-04-26 21:17:06 +0800

    Projection Submission (Edited)

--- ui.R
+++ ui.R
@@ -1,7 +1,12 @@
 library(shiny)
 
 shinyUI(fluidPage(
+    
     h2("Inch to Centimetre Converter"),
+    
+    h3("This shiny application converts inches to centimetres."),
+    h3("Just input the number of inches that you would like to convert 
+       in the first box and click convert to get the equivalent amount in centimetres in the second box."),
     
     numericInput('value',label=h3("Inch"),1,
                      min=0),
